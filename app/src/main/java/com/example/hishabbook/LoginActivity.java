3ec7e4ff0ca3c4a57cd8824d9e97d3936a48640e
rone458 <ronedoshi123@gmail.com>
2021-02-04 04:36:32 -0800

    Login Activity added with firebase

--- app/src/main/java/com/example/hishabbook/LoginActivity.java
+++ app/src/main/java/com/example/hishabbook/LoginActivity.java
@@ -1,14 +1,62 @@
 package com.example.hishabbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private EditText email,password;
+    private Button login;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email_et);
+        password = findViewById(R.id.pass_et);
+
+        mAuth = FirebaseAuth.getInstance();
+        String user_email = email.getText().toString().trim();
+        String user_pass = password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(user_email,user_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.d(TAG,"SignInwithEmailandPassword: success");
+                    Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+                }else{
+                    Log.w(TAG,"signInWithEmailandPassword: failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (user != null){
+             user = mAuth.getCurrentUser();
+        }
     }
 }
