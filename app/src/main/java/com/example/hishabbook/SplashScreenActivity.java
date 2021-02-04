1cdedcf0272004a263e747349c03d4d783454ec9
rone458 <ronedoshi123@gmail.com>
2021-02-04 03:29:11 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hishabbook/SplashScreenActivity.java
@@ -0,0 +1,46 @@
+package com.example.hishabbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH = 3000;
+    Animation animation;
+    private ImageView imageSplash;
+    private TextView appName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+
+        imageSplash = findViewById(R.id.image_splash);
+        appName = findViewById(R.id.appName);
+
+        imageSplash.setAnimation(animation);
+        appName.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
+
+    }
+}
